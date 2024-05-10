0f0a9e8ff60bf0b4164b45cb6808c2a881981fe3
Nathan Maciel <112430114+nathanmacielviana@users.noreply.github.com>
2024-05-10 10:41:29 -0300

    Initial commit

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./navbar.css";
+
+/*Primeira função*/
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+      <span>React Kanban</span>
+    </nav>
+  );
+}
